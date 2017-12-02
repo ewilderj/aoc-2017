b6061f7f4bec2e5feaa9a1ac15fddcb5b763a778
Edd Wilder-James <edd@me.com>
2017-12-02 13:37:01 -0800

    Switch to threading macro instead of nesting

--- 02/day2/src/day2/core.clj
+++ 02/day2/src/day2/core.clj
@@ -3,26 +3,40 @@
             [clojure.math.combinatorics :as combo]))
 
 (def inp
-  (map (fn [c] (map #(Integer/parseInt %) c))
-       (map #(str/split % #"\t")
-            (str/split-lines (slurp "/Users/edd/work/github/aoc-2017/02/day2/puzzle.txt")))))
+  (->> (slurp "/Users/edd/work/github/aoc-2017/02/day2/puzzle.txt")
+       (str/split-lines)
+       (map #(str/split % #"\t"))
+       (map (fn [c] (map #(Integer/parseInt %) c)))))
 
 (println "part1 " (reduce + (map #(- (apply max %) (apply min %)) inp)))
 
 (defn cx [c]
-  (apply / (flatten (filter #(= 0 (apply mod %)) (map (partial sort >) (combo/combinations c 2))))))
+  (->> (combo/combinations (sort > c) 2)
+   (filter #(= 0 (apply mod %)))
+   (flatten)
+   (apply /)))
 
 (println "part2 " (reduce + (map cx inp)))
 
 ;; EXTENSION EXERCISE: do it without non-core libraries
 ;; if you think using combo/combinations is cheating, here's
-;; a definition of it just for sorted pairwise combinations
+;; a definition of it just for pairwise combinations
 
-(defn pairwise-combinations [c]
+(defn pairwise-combinations-o [c]
   (partition 2 (flatten (map (fn [[x y]] (map #(sort > (list x %)) y))
                              (drop-last (map-indexed (fn [n x] (list x (drop (inc n) c))) c))))))
 
+(defn pairwise-combinations [c]
+  (->> (map-indexed (fn [n x] (list x (drop (inc n) c))) c)
+       (drop-last)
+       (map (fn [[x y]] (map #(sort > (list x %)) y)))
+       (flatten)
+       (partition 2)))
+
 (defn px [c]
-  (apply / (flatten (filter #(= 0 (apply mod %)) (pairwise-combinations c)))))
+  (->> (pairwise-combinations (sort > c))
+       (filter #(= 0 (apply mod %)))
+       (flatten)
+       (apply /)))
 
 (println "part2-ex " (reduce + (map px inp)))
