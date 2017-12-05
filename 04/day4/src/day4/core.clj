7fb563b5d6b9c916d6b691d9674c9f7f406f2647
Edd Wilder-James <edd@me.com>
2017-12-04 16:10:29 -0800

    Added more 'readable' version

--- 04/day4/src/day4/core.clj
+++ 04/day4/src/day4/core.clj
@@ -1,13 +1,28 @@
 (ns day4.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.walk :as w]))
 
-(def inp (str/split-lines (slurp "/Users/edd/work/github/aoc-2017/04/day4/puzzle.txt")))
+(def codes (map #(str/split % #" ") (str/split-lines (slurp "/Users/edd/work/github/aoc-2017/04/day4/puzzle.txt"))))
 
 (println "part1"
-         (reduce + (map (fn [l] (if (empty? (filter #(> (second %) 1) (frequencies (str/split l #" ")))) 1 0)) inp)))
+         (reduce + (map (fn [l] (if (empty? (filter #(> (second %) 1) (frequencies l))) 1 0)) codes)))
 
 (println "part2"
-         (reduce + (map (fn [l] (if (empty? (filter #(> (second %) 1) (frequencies (map (comp set frequencies) (str/split l #" "))))) 1 0)) inp)))
+         (reduce + (map (fn [l] (if (empty? (filter #(> (second %) 1) (frequencies (map (comp set frequencies) l)))) 1 0)) codes)))
 
 
-         
+;; threaded part 1
+(def t1 (->> codes
+             (map frequencies)
+             (map (partial filter #(> (second %) 1)))
+             (filter empty?)
+             (count)))
+
+;; threaded part 2
+(def t2 (->> codes
+             (map (partial map (comp set frequencies)))
+             (map frequencies)
+             (map (partial filter #(> (second %) 1)))
+             (filter empty?)
+             (count)))
+
