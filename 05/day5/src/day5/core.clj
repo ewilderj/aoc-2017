53ed0e659e9bc5c1a7d748d4970e5bb2c3a47007
Edd Wilder-James <edd@me.com>
2017-12-05 09:01:46 -0800

    Remove debug

--- 05/day5/src/day5/core.clj
+++ 05/day5/src/day5/core.clj
@@ -18,8 +18,9 @@
 
 (defn move2 [c]
   (loop [ins c pc 0 n 0]
-    ;; (if (= 0 (mod n 5000)) (println "pc" pc))
     (if (>= pc (count ins)) n
         (let [i (nth ins pc)]
           (recur (update ins pc (if (>= i 3) dec inc)) (+ pc i) (inc n))))))
 
+(defn part2 [] (println (move2 prog)))
+;; -> 28372145
