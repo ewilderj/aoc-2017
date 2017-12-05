1978ce4fb62c579b1293520c9eb0e1dd617637e8
Edd Wilder-James <edd@me.com>
2017-12-05 09:01:01 -0800

    Day 5 answers

--- 05/day5/src/day5/core.clj
+++ 05/day5/src/day5/core.clj
@@ -2,7 +2,24 @@
   (:require [clojure.string :as str]))
 
 (def prog
-  ->> (slurp "/Users/edd/work/github/aoc-2017/05/day5/puzzle.txt")
+  (->> (slurp "/Users/edd/work/github/aoc-2017/05/day5/puzzle.txt")
   (str/split-lines)
-  (map #(Integer/parseInt %)))
+  (map #(Integer/parseInt %))
+  (vec)))
+
+(defn move [c]
+  (loop [ins c pc 0 n 0]
+    (if (>= pc (count ins)) n
+        (let [i (nth ins pc)]
+          (recur (update ins pc inc) (+ pc i) (inc n))))))
+
+(defn part1 [] (println (move prog)))
+;; -> 356945
+
+(defn move2 [c]
+  (loop [ins c pc 0 n 0]
+    ;; (if (= 0 (mod n 5000)) (println "pc" pc))
+    (if (>= pc (count ins)) n
+        (let [i (nth ins pc)]
+          (recur (update ins pc (if (>= i 3) dec inc)) (+ pc i) (inc n))))))
 
