123a89eda40236e04abf9c1936c3b663cdfbe5ee
Edd Wilder-James <edd@me.com>
2017-12-10 13:04:23 -0800

    Day 9, with spec

---
+++ 09/day9/src/day9/core.clj
@@ -0,0 +1,51 @@
+(ns day9.core
+  (:require [clojure.string :as str]
+            [clojure.spec.alpha :as s]
+            [clojure.java.io :as io]))
+
+;; An exercise in spec
+;; Doesn't perform so quickly!
+
+(defn remove-ignored [s] (apply concat (str/split s #"!.")))
+
+(def inp (str/trim (slurp "/Users/edd/work/github/aoc-2017/09/day9/puzzle.txt")))
+
+(defn random? [c] (not= c \>))
+(s/def ::garbage (s/cat :s #{\<} :u (s/* random?) :e #{\>}))
+(s/def ::content (s/cat :d (s/alt :group ::group :garbage ::garbage)
+                        :m (s/? #{\,})))
+(s/def ::group (s/cat :x #{\{} :c (s/* ::content) :y #{\}}))
+
+(def test-garbage ["<>", "<jshdkjdhsf>", "<<<<>", "<{!>}>", "<!!>",
+                   "<!!!>>", "<{o\"i!a,<{i<a>"])
+
+(assert (every? #{true} (map #(s/valid? ::garbage (remove-ignored %)) test-garbage)))
+
+(def test-groups ["{}", "{{{}}}", "{{},{}}",
+                  "{{{},{},{{}}}}", "{<a>,<a>,<a>,<a>}",
+                  "{{<ab>},{<ab>},{<ab>},{<ab>}}",
+                  "{{<!!>},{<!!>},{<!!>},{<!!>}}",
+                  "{{<a!>},{<a!>},{<a!>},{<ab>}}"])
+
+(assert (every? #{true} (map #(s/valid? ::group (remove-ignored %)) test-groups)))
+
+(defn count-in [g n]
+  (conj
+   (if-let [c (:c g)]
+     (->> c
+          (map :d)
+          (filter #(#{:group} (first %)))
+          (map #(count-in (second %) (inc n)))
+          )) n))
+
+(defn part1 []
+  (reduce + (flatten (count-in (s/conform ::group (remove-ignored inp)) 1))))
+
+(defn count-garbage [g]
+  (let [gf (fn [[t m]] (if (= :group t) (count-garbage m) (count (:u m))))
+        c (:c g)]
+  (if (nil? c) (list 0)
+    (->> c (map :d) (filter #(#{:group :garbage} (first %))) (map gf)))))
+
+(defn part2 []
+  (reduce + (flatten (count-garbage (s/conform ::group (remove-ignored inp))))))
---
+++ 09/day9/test/day9/core_test.clj
@@ -0,0 +1,7 @@
+(ns day9.core-test
+  (:require [clojure.test :refer :all]
+            [day9.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
